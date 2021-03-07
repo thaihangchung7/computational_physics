a12beb9a754a55bb88ad2b95cbe0815e9659a675
Thaihang Chung <thaihangchung7@gmail.com>
2021-03-07 16:54:00 -0500

    updated animation for lorrenz

--- lorrenzattractor/lattractor.py
+++ lorrenzattractor/lattractor.py
@@ -21,14 +21,39 @@ state0 = [1.0,1.0,1.0]
 t = np.arange(0.0,40.0,0.01)
 statet = odeint(lorrenz,state0,t)
 
-print(statet)
-
+print(statet[:,0][:7])
+"""
 fig = plt.figure()
 ax = fig.gca(projection="3d")
+line, = ax.plot([],[],[])
+"""
+fig = plt.figure()
+ax = fig.add_subplot(111)
+line, = ax.plot(statet[:,0],statet[:,1],lw=2)
+
+def animate(i):
+    x=statet[:,0]
+    y=statet[:,1]
+
+    line.set_data(x[:i],y[:i])
+    return line,
+ani = ani.FuncAnimation(fig, animate, len(statet[:,0]), interval = 100, blit=False)
+
+#plt.plot(statet[:,0],statet[:,1])
+ani.save('2dlorrenz.gif')
+plt.show()
+
+"""
+ax = fig.gca(projection="3d")
 ax.plot(statet[:, 0], statet[:, 1], statet[:, 2])
-#make this work?
-#animator = ani.FuncAnimation(fig,,interval=100)
 plt.draw()
 plt.show()
 
-
+#for updating frames
+def update(i,factor):
+    line.set_xdata(state[:,0])
+    line.set_ydata(state[:,1])
+    line.set_zdata(state[:,2])
+ 
+    return line
+"""

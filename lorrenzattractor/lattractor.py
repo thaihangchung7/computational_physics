d70950ab5fa49c31a3de21ce0ca358ae51e9832f
Thai Chung <thaichung@pop-os.localdomain>
2021-03-14 17:44:38 -0400

    added 2dplot

--- lorrenzattractor/lattractor.py
+++ lorrenzattractor/lattractor.py
@@ -21,11 +21,9 @@ state0 = [1.0,1.0,1.0]
 t = np.arange(0.0,40.0,0.01)
 statet = odeint(lorrenz,state0,t)
 
-print(len(statet[:,0]))
-print(len(statet[:,1]))
-print(len(statet[:,2]))
-
-
+#print(len(statet[:,0]))
+#print(len(statet[:,1]))
+#print(len(statet[:,2]))
 
 """
 fig = plt.figure()
@@ -33,32 +31,32 @@ ax = fig.gca(projection="3d")
 line, = ax.plot([],[],[])
 """
 fig = plt.figure()
-"""
 ax = fig.add_subplot(111)
 line, = ax.plot(statet[:,0],statet[:,1],lw=2)
+
 """
+#3d stuff
 ax3d = fig.gca(projection="3d")
 line3d, = ax3d.plot(statet[:,0],statet[:,1],statet[:2],lw=2)
-
+"""
 
 def animate(i):
     x=statet[:,0]
     y=statet[:,1]
-
     line.set_data(x[:i],y[:i])
     return line,
-"""
+
 def animate3d(i):
     x=statet[:,0]
     y=statet[:,1]
     z=statet[:,2]
     line3d.set_data(x[:i],y[:i],z[:i])
     return line3d,
-"""
-#ani = ani.FuncAnimation(fig, animate, len(statet[:,0]), interval = 5, blit=False)
-ani3d  = ani.FuncAnimation(fig, animate3d, len(statet[:,0]), interval = 5, blit=False)
-#plt.plot(statet[:,0],statet[:,1])
-#ani.save('2dlorrenz.gif')
+
+ani = ani.FuncAnimation(fig, animate, len(statet[:,0]), interval = 5, blit=False)
+#ani3d  = ani.FuncAnimation(fig, animate3d, len(statet[:,0]), interval = 5, blit=False)
+plt.plot(statet[:,0],statet[:,1])
+ani.save('2dlorrenz.gif', writer='ffmpeg')
 plt.show()
 
 """
